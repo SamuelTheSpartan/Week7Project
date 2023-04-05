43bbfa6e3240065da79f948a14d99add2e215914
Samuel Burton <sburton@spartaglobal.com>
2023-04-05 15:22:34 +0100

    adding base project

---
+++ LumaAutomatedWebTestingProject/LumaAutomatedWebTestingProject/UnitTest1.cs
@@ -0,0 +1,17 @@
+namespace LumaAutomatedWebTestingProject
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
