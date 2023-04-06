f22efa7b7a42248f14aae686e53d3922bcf36b26
Glen Smith <gsmith@spartaglobal.com>
2023-04-06 15:25:12 +0100

    Added to Login Def pages

--- LumaAutomatedWebTestingProject/LumaAutomatedWebTestingProject/lib/pages/SL_Website.cs
+++ LumaAutomatedWebTestingProject/LumaAutomatedWebTestingProject/lib/pages/SL_Website.cs
@@ -4,16 +4,22 @@ using OpenQA.Selenium;
 namespace LumaAutomatedWebTestingProject.lib.pages
 {
     public class SL_Website<T> where T : IWebDriver, new()
+
     {
         #region Accessible Page Objects and Driver
         public IWebDriver SeleniumDriver { get; set; }
         public SL_HomePage SL_HomePage { get; set; }
+
+        public SL_LoginPage SL_LoginPage { get; set; }
+
+
         #endregion
 
         public SL_Website(int pageLoadInSecs = 10, int implicitlyWaitInSecs = 10, bool isHeadless = false)
         {
             SeleniumDriver = new SeleniumDriverConfig<T>(pageLoadInSecs, implicitlyWaitInSecs, isHeadless).Driver;
             SL_HomePage = new SL_HomePage(SeleniumDriver);
+            SL_LoginPage = new SL_LoginPage(SeleniumDriver);
         }
     }
 }
