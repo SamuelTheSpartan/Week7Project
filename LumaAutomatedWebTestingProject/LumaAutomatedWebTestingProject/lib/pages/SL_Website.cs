db1691e9b7bc5d48e1742ee838ddfedbbafecd14
Lucas Brennan <lucasjacbrennan@gmail.com>
2023-04-06 11:03:30 +0100

    Framework Extention

---
+++ LumaAutomatedWebTestingProject/LumaAutomatedWebTestingProject/lib/pages/SL_Website.cs
@@ -0,0 +1,19 @@
+﻿using LumaAutomatedWebTestingProject.lib.driver_config;
+using OpenQA.Selenium;
+
+namespace LumaAutomatedWebTestingProject.lib.pages
+{
+    public class SL_Website<T> where T : IWebDriver, new()
+    {
+        #region Accessible Page Objects and Driver
+        public IWebDriver SeleniumDriver { get; set; }
+        public SL_HomePage SL_HomePage { get; set; }
+        #endregion
+
+        public SL_Website(int pageLoadInSecs = 10, int implicitlyWaitInSecs = 10, bool isHeadless = false)
+        {
+            SeleniumDriver = new SeleniumDriverConfig<T>(pageLoadInSecs, implicitlyWaitInSecs, isHeadless).Driver;
+            SL_HomePage = new SL_HomePage(SeleniumDriver);
+        }
+    }
+}
